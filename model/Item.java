3dfbe4fe5ee9d676a5ef65b5dd82df9179fa8545
serge <sergei.g.d@gmail.ccom>
2022-09-03 13:54:48 +0700

    готовое

---
+++ model/Item.java
@@ -0,0 +1,14 @@
+package pro.sky.skyprospringwebbasket.model;
+
+public class Item {
+
+    private final int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public Item(int id) {
+        this.id = id;
+    }
+}
