b79c4753928414905503cb2099b908c000aa1bfc
serge <sergei.g.d@gmail.ccom>
2022-09-03 13:46:29 +0700

    1

---
+++ SkyproSpringWebbasketApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringwebbasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringWebbasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringWebbasketApplication.class, args);
+	}
+
+}
